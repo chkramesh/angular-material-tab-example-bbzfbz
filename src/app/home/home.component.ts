137924588a8942ba5c7faea226a86a7bf27d644a
chkramesh <chkramesh@gmail.com>
2019-05-02 14:42:35 -0400

    3 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  disableTextbox =  false;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: [{ value: 'a', disabled: true }],
+      street: [''],
+      zip: [''],
+    });
+  }
 
   ngOnInit() {
+    this.form.patchValue({
+      name: 'name',
+      lastName: 'Last Name'
+    });
+
+    // this.form.disable();
+  }
+
+  update() {
+    console.log(this.form.value);
+
+    this.disableTextbox = !this.disableTextbox;
+    // this.disableTextbox = true;;
   }
 
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
